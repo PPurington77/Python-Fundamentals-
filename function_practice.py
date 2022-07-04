b2c4fea5364939981d1284634a0a0d4e274d7dd5
PPurington77 <papurington@gmail.com>
2022-07-04 12:57:58 -0600

    completed function basic 1

---
+++ function_practice.py
@@ -0,0 +1,36 @@
+# def add(a, b):
+#     x = a + b
+#     return(x)
+
+# new_val = add(5, 10)
+# print(new_val)
+
+# def say_hi(name):
+#     print("Hi " + name)
+
+# say_hi("Patrick")
+
+# def say_hi(name):
+#     return "Hi " + name
+
+# greeting = say_hi("Patrick")
+# print(greeting)
+
+# def full_name(first_name, last_name):
+#     return first_name + last_name
+
+# name1 = full_name("Patrick ", "Purington")
+# print(name1)
+
+# def be_cheerful(name='', repeat=2):
+#     print(f"Good Morning {name}\n" * repeat)
+
+# be_cheerful(repeat=3, name="kb")
+
+# def multiply(num_list, num):
+#     for x in range(len(num_list)):
+#         num_list[x] *= num
+#     return num_list
+# a = [2, 4, 10, 16]
+# b = multiply(a, 5)
+# print(b)
